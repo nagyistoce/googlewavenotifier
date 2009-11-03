99a94d071e5be6b4d0449c4bf93fe276dae115a5
frozencow <frozencow@82880924-c33d-11de-a4c1-8109ed5598a0>
2009-11-03 00:39:39 +0000

    Updated File version of GoogleWaveNotifier.

--- trunk/GoogleWaveNotifier/Properties/AssemblyInfo.cs
+++ trunk/GoogleWaveNotifier/Properties/AssemblyInfo.cs
@@ -38,4 +38,4 @@ using GoogleWaveNotifier;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
