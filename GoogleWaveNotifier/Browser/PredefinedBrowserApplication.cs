92a0d6f16825bfb90cc1af0619f8333f2d24ea8a
frozencow <frozencow@82880924-c33d-11de-a4c1-8109ed5598a0>
2009-12-17 13:12:27 +0000

    Changed the way browsers are detected.

--- GoogleWaveNotifier/Browser/PredefinedBrowserApplication.cs
+++ GoogleWaveNotifier/Browser/PredefinedBrowserApplication.cs
@@ -4,22 +4,14 @@ namespace GoogleWaveNotifier.Browser
 {
     public class PredefinedBrowserApplication : CommandLineBrowserApplication
     {
-        public string Executable { get; set; }
+        public string RegistryLocation { get; set; }
         public bool IsInstalled
         {
             get { return GetCommandLine() != null; }
         }
         protected override string GetCommandLine()
         {
-            var commandKey = Registry.ClassesRoot.OpenSubKey(@"Applications\" + Executable + @"\shell\open\command");
-            if (commandKey != null)
-            {
-                using (commandKey)
-                {
-                    return commandKey.GetValue(null) as string;
-                }
-            }
-            return null;
+            return Registry.GetValue(RegistryLocation, null, null) as string;
         }
     }
 }
