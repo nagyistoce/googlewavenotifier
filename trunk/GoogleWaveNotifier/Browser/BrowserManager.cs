c078050d69642a306570190d8aa2ee97848a3b1e
frozencow <frozencow@82880924-c33d-11de-a4c1-8109ed5598a0>
2009-12-21 15:54:02 +0000

    Made use of HKEY_LOCAL_MACHINE\SOFTWARE\Clients\StartMenuInternet\ to check for installed browsers. Removed PredefinedBrowser mechanism.

--- trunk/GoogleWaveNotifier/Browser/BrowserManager.cs
+++ trunk/GoogleWaveNotifier/Browser/BrowserManager.cs
@@ -1,48 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using Microsoft.Win32;
 
 namespace GoogleWaveNotifier.Browser
 {
     public static class BrowserManager
     {
-        public static ICollection<PredefinedBrowserApplication> PredefinedBrowsers { get; private set; }
-        private static PredefinedBrowserApplication[] _installedBrowsers;
-        public static ICollection<PredefinedBrowserApplication> InstalledBrowsers
+        private static InstalledBrowserApplication[] _installedBrowsers;
+        public static ICollection<InstalledBrowserApplication> InstalledBrowsers
         {
             get
             {
                 if (_installedBrowsers != null)
                     return _installedBrowsers;
-                return _installedBrowsers = (from b in PredefinedBrowsers where b.IsInstalled select b).ToArray();
+                return _installedBrowsers = GetInstalledBrowsers();
             }
         }
+
         public static IBrowserApplication DefaultBrowser { get; private set; }
 
         static BrowserManager()
         {
             DefaultBrowser = new DefaultBrowserApplication();
-            PredefinedBrowsers = new[]
-                                     {
-                                         new PredefinedBrowserApplication { Name = "Firefox", RegistryLocation = @"HKEY_CLASSES_ROOT\FirefoxURL\shell\open\command" },
-                                         new PredefinedBrowserApplication { Name = "Google Chrome", RegistryLocation = @"HKEY_CLASSES_ROOT\ChromeHTML\shell\open\command" },
-                                         new PredefinedBrowserApplication { Name = "Opera", RegistryLocation = @"HKEY_CLASSES_ROOT\Opera.HTML\shell\open\command" },
-                                         new PredefinedBrowserApplication { Name = "Internet Explorer", RegistryLocation = @"HKEY_CLASSES_ROOT\htmlfile\shell\open\command" },
-                                         new PredefinedBrowserApplication { Name = "Safari", RegistryLocation = @"HKEY_CLASSES_ROOT\SafariURL\shell\open\command" }
-                                     };
         }
 
-        public static PredefinedBrowserApplication GetPredefinedBrowser(string name)
+        private static InstalledBrowserApplication[] GetInstalledBrowsers()
         {
-            return (from b in PredefinedBrowsers where b.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase) select b).FirstOrDefault();
+            var browsers = new List<InstalledBrowserApplication>();
+            using (var browsersKey = Registry.LocalMachine.OpenSubKey(@"Software\Clients\StartMenuInternet"))
+            {
+                if (browsersKey != null)
+                {
+                    foreach (var browser in browsersKey.GetSubKeyNames())
+                    {
+                        var installedBrowser = new InstalledBrowserApplication(browser);
+                        if (installedBrowser.HasCommandLine)
+                            browsers.Add(new InstalledBrowserApplication(browser));
+                    }
+                }
+            }
+            return browsers.ToArray();
         }
 
-        public static PredefinedBrowserApplication GetInstalledBrowser(string name)
+        public static InstalledBrowserApplication GetInstalledBrowser(string name)
         {
-            PredefinedBrowserApplication predefined = GetPredefinedBrowser(name);
-            if (predefined == null)
-                return null;
-            return predefined.IsInstalled ? predefined : null;
+            InstalledBrowserApplication installedBrowser = new InstalledBrowserApplication(name);
+            return installedBrowser.HasCommandLine ? installedBrowser : null;
         }
     }
 }
